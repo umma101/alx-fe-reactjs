d82408169f0a40a76dd330d889e89c6c2832883d
Mary <maryauma2606@gmail.com>
2025-07-11 15:43:19 +0300

    add alx-react-app

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+export default App;
