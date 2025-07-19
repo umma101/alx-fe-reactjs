074c5a2a84ea1dcc7ae6623eda1ab2c7be629117
Mary <maryauma2606@gmail.com>
2025-07-19 14:50:50 +0300

    Add interactive contact page with inline styling

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Contact from './pages/Contact';
+import Contact from './components/Contact';
 
 function App() {
   return (
