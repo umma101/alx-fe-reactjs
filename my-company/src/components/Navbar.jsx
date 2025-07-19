c448b6815daa9fc1d4d1d4c60a01640980423456
Mary <maryauma2606@gmail.com>
2025-07-19 14:53:53 +0300

    Add interactive contact page with inline styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#eee' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#eee' ,display: 'flex', justifyContent: 'space-around'}}>
       <Link to="/" style={{ margin: '10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '10px' }}>About</Link>
       <Link to="/services" style={{ margin: '10px' }}>Services</Link>
