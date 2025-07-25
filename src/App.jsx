8ec6422574453209661475da80651a90511ab4f5
WARREN WANYAMA <WWANYAMA04@GMAIL.COM>
2025-07-25 15:04:05 +0200

    Initial commit - React Zustand Recipe App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetail from './components/RecipeDetail';
-import EditRecipeForm from './components/EditRecipeForm';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
     <Router>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/add" element={<AddRecipeForm />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="/edit/:id" element={<EditRecipeForm />} />
-      </Routes>
+      <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+        <h1>🍲 Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddRecipeForm />
+              <hr />
+              <RecipeList />
+            </>
+          } />
+        </Routes>
+      </div>
     </Router>
   );
 }
