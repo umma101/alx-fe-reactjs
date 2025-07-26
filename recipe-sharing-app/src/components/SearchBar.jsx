4111bd65834c215e71d10216bccf0b659c0767ed
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-07-26 17:10:27 +0300

    Added Searchbar.jsx file

---
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+// src/components/SearchBar.jsx
+import useRecipeStore from './recipeStore';
+
+const SearchBar = () => {
+  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
+  
+  return (
+    <input
+      type="text"
+      onChange={(e) => setSearchTerm(e.target.value)}
+      placeholder="Search recipes..."
+    />
+  );
+};
+
+export default SearchBar; 
