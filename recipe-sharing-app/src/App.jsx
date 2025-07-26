84d8658ca4504042fd979961e994cd659594ca59
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-07-26 17:16:43 +0300

    Added routing for RecipeDetail in App.jsx file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
               <RecipeList />
             </>
           } />
+          {/* 2. Add the new route for recipe details */}
+          <Route path="/recipes/:id" element={<RecipeDetail />} />
         </Routes>
       </div>
     </Router>
