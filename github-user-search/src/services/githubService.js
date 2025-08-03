b534b1083c1ae93de1fa7051e355548696c18886
Mary <maryauma2606@gmail.com>
2025-08-03 16:55:02 +0300

    add

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-export const fetchUsersByQuery = async (query) => {
+export const fetchUsersByQuery = async (query, location = '', minRepos = 0) => {
   try {
-    const response = await axios.get(`${BASE_URL}/search/users?q=${encodeURIComponent(query)}`);
+    const searchQuery = `${query}+location:${location}+repos:>${minRepos}`; // includes "location" and "minRepos"
+    const response = await axios.get(`${BASE_URL}/search/users?q=${encodeURIComponent(searchQuery)}`);
     return response.data.items;
   } catch (error) {
     throw new Error('Failed to fetch users');
